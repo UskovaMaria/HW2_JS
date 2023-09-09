1d0045c124aebb0f5d6e5ef10e0935842af4cd4d
Uskova Maria <uskovamaria16@gmail.com>
2023-09-09 12:11:35 +0200

    correction of mistakes

--- js/main.js
+++ js/main.js
@@ -13,20 +13,19 @@
 // let a = 5;
 // a = a + 3;
 
-let action1 = 'Баланс';
-let action2 = 'Покласти';
-let action3 = 'Зняти';
+let action1 = '1';
+let action2 = '2';
+let action3 = '3';
 let balance = +'1000';
 
-let userAction = prompt('Вибрати дію');
+let userAction = prompt('Вибрати дію: 1. Баланс; 2. Покласти; 3. Зняти;');
   
-let userSum = +prompt('Сума:');
-let res = balance + userSum;
-let res2 = balance - userSum;
+let userSum;
 
-console.log('Action 1: ', action1);
-console.log('Action 2: ', action2);
-console.log('Action 3: ', action3);
+
+console.log('Баланс: ', action1);
+console.log('Покласти: ', action2);
+console.log('Зняти: ', action3);
 
 if (userAction === action1) {
   console.log('Баланс:', balance);
@@ -34,12 +33,14 @@ if (userAction === action1) {
 
 if (userAction === action2) {
   userSum = +prompt('Сума:');
-  console.log('Сума: ', res);
+  balance = balance + userSum;
+  console.log('Сума: ', balance);
 }
 
 if (userAction === action3) {
-  userAction = prompt('Вибрати дію');
+  userAction = prompt('Вибрати дію: 1. Баланс; 2. Покласти; 3. Зняти;');
   console.log('Баланс:', balance);
   userSum = +prompt('Сума:');
-  console.log('Сума: ', res2);
+  balance = balance - userSum;
+  console.log('Сума: ', balance);
 }
